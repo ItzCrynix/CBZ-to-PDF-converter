83655b278a176e9aa876f26654dffb6b9b7d19bb
ItzCrynix <cainanloyola376@gmail.com>
2026-02-21 19:33:54 -0300

    Created function to convert a directory of files

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from converter import CBZConverter
+
+if __name__ == "__main__":
+    response = input("Please, write the file/directory you want to convert: ")
+
+    converter = CBZConverter()
+
+    if os.path.isdir(response):
+        files = []
+        for _, _, filenames in os.walk(response):
+            files.extend(filenames)
+            break
+
+        converted_files = 0
+        total_files = len(files)
+        for file in files:
+            print(f'Converting: {file} ({converted_files}/{total_files})\n')
+            if converter.convert_zip_to_pdf(f'{response}/{file}'):
+                converted_files += 1
+    else:
+        converter.convert_zip_to_pdf(response)
